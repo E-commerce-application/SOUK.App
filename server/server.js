db96ab1445c46694d40e752a22fc1eb9451958d5
Habiba-Ben-Nasr <bennasrhabiba@gmail.com>
2021-12-27 21:06:06 +0100

    define models

---
+++ server/server.js
@@ -0,0 +1,20 @@
+require("dotenv").config({ path: "./config/.env" });
+
+const express = require("express");
+const connectDB = require("./config/connectDB");
+
+
+const app = express();
+
+app.use(express.json());
+
+connectDB();
+
+
+
+// Server connection
+app.listen(process.env.PORT, (err) => {
+  err
+    ? console.log("Server connection failed", err)
+    : console.log(`💻 is connected on 🚪 ${process.env.PORT}`);
+});
