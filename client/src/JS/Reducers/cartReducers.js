5bd5217167d1db515a42b0dcdad19d14d85860aa
Mohamed Ali <medali.bnasr@gmail.com>
2022-01-10 10:52:55 +0100

    update

--- client/src/JS/Reducers/cartReducers.js
+++ client/src/JS/Reducers/cartReducers.js
@@ -1,68 +1,19 @@
-import {
-  LOG_OUT,
-  USER_LOGIN,
-  USER_LOGIN_FAILED,
-  USER_LOGIN_SUCCESS,
-  USER_REGISTER,
-  USER_REGISTER_FAILED,
-  USER_REGISTER_SUCCESS,
-} from "../ActionTypes/userActionsTypes.js";
+import { ADD_PRODUCT } from "../ActionTypes/cartActionTypes";
 
 const initialState = {
-  loading: false,
-  currentUser: null,
-  errors: null,
-  isAuth: false,
-  err:false,
+  products: [],
+  quantity: 0,
+  total: 0,
 };
 
-const userReducer = (state = initialState, { type, payload }) => {
+const cartReducers = (state = initialState, { type, payload }) => {
   switch (type) {
-    case USER_REGISTER:
+    case ADD_PRODUCT:
       return {
         ...state,
-        loading: true,
-      };
-    case USER_LOGIN:
-      return {
-        ...state,
-        loading: true,
-      };
-
-    case USER_REGISTER_SUCCESS:
-      return {
-        ...state,
-        loading: false,
-        currentUser: payload,
-      };
-
-    case USER_REGISTER_FAILED:
-      return {
-        ...state,
-        loading: false,
-        errors: payload,
-        err:true,
-      };
-    case USER_LOGIN_FAILED:
-      return {
-        ...state,
-        loading: false,
-        errors: payload,
-        err:true,
-      };
-
-    case USER_LOGIN_SUCCESS:
-      return {
-        ...state,
-        loading: false,
-        isAuth: true,
-        msg: payload,
-      };
-
-    case LOG_OUT:
-      return {
-        ...state,
-        isAuth: false,
+        quantity: state.quantity + 1,
+        products: [...state.products, payload],
+        total: state.total + payload.price * payload.quantity,
       };
 
     default:
@@ -70,4 +21,4 @@ const userReducer = (state = initialState, { type, payload }) => {
   }
 };
 
-export default userReducer;
+export default cartReducers;
